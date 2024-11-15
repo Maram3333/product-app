cb04b846ed3104ee75e59380e2c21a583c94272b
Maram3333 <maramarfaoui316@gmail.com>
2024-11-15 22:42:14 +0100

    first commit

---
+++ src/product.js
@@ -0,0 +1,9 @@
+// product.js
+const product = {
+    name: "Wireless Headphones",
+    price: "$120",
+    description: "High-quality wireless headphones with noise-cancellation features.",
+    image: "https://via.placeholder.com/150"
+};
+
+export default product;
